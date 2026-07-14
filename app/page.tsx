eb68f3fca60fac004213ab5299b4fda79d87198c
Vihaan Kerekatte <vkerekatte@gmail.com>
2026-07-13 20:17:26 -0400

    setup: example

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex flex-1 items-center justify-center">
-      <h1 className="text-2xl font-semibold">HackNC 2026</h1>
+      <h1 className="text-2xl font-semibold">HackNC 2026!</h1>
     </div>
   );
 }
