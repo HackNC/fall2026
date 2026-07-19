b8a8269eb7b49bf7924724692ca081a4791654bb
Rachel Lin <linr@unc.edu>
2026-07-18 23:13:07 -0400

    feat: added data for faq and sponsors

---
+++ data/sponsors.ts
@@ -0,0 +1,45 @@
+export type Sponsor = {
+  name: string;
+  logoSrc: string;
+  alt: string;
+  width: number;
+  height: number;
+};
+
+export const sponsors: Sponsor[] = [
+  {
+    name: "Sponsor 1",
+    logoSrc: "/sponsor_placeholder.svg",
+    alt: "Sponsor logo placeholder",
+    width: 280,
+    height: 100,
+  },
+  {
+    name: "Sponsor 2",
+    logoSrc: "/sponsor_placeholder.svg",
+    alt: "Sponsor logo placeholder",
+    width: 220,
+    height: 100,
+  },
+  {
+    name: "Sponsor 3",
+    logoSrc: "/sponsor_placeholder.svg",
+    alt: "Sponsor logo placeholder",
+    width: 260,
+    height: 100,
+  },
+  {
+    name: "Sponsor 4",
+    logoSrc: "/sponsor_placeholder.svg",
+    alt: "Sponsor logo placeholder",
+    width: 240,
+    height: 100,
+  },
+  {
+    name: "Sponsor 5",
+    logoSrc: "/sponsor_placeholder.svg",
+    alt: "Sponsor logo placeholder",
+    width: 300,
+    height: 100,
+  },
+];
